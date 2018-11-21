534feb1fa7aba70f4f48c6cbe4b69bd23d70778d
Uri Shaked <uri@urishaked.com>
2018-11-21 15:48:12 +0200

    feat: add input and a counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ivy-jit-playground';
+  counter = 0;
+
+  startCounter() {
+    this.counter = 1;
+    setInterval(() => this.counter++, 1000);
+  }
 }
